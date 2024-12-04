ae81a56b0892e4deaedcf86cffc483a377ba8bd1
Kamsi Agbo <110093666+chriskamsi2000@users.noreply.github.com>
2024-12-04 14:26:17 +0100

    Add files via upload

---
+++ assign.php
@@ -0,0 +1,17 @@
+<?php
+$name = "Kamsi";
+echo "$name is a good boy";
+echo "<br>";
+echo "<br>";
+echo "$name is also a tall boy";
+echo "<br>";
+$scare= "5b";
+$isAdmin= false;
+$fruits= ["apple","banana"];
+echo "<br>";
+echo 'Apple';
+echo "<br>";
+echo "<br>";
+var_dump ($fruits);
+echo $isAdmin
+?>
